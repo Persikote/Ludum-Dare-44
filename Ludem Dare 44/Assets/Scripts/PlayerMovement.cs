b4677d2d480e0da5f60d6d54a17ae80b0d062914
Persikoté <30988624+ThePersika@users.noreply.github.com>
2019-04-27 12:53:07 +0200

    Enable player movement

---
+++ Ludem Dare 44/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller))]
+public class PlayerMovement : MonoBehaviour
+{
+	[SerializeField] private float moveSpeed;
+	[SerializeField] private float gravity;
+	[SerializeField] private float maxJumpHeight;
+	[SerializeField] private float minJumpHeight;
+	private float maxJumpVelocity;
+	private float minJumpVelocity;
+	private Vector2 velocity;
+	private Controller controller;
+
+	private void Start()
+	{
+		controller = GetComponent<Controller>();
+
+		maxJumpVelocity = Mathf.Sqrt(2 * gravity * maxJumpHeight);
+		minJumpVelocity = Mathf.Sqrt(2 * gravity * minJumpHeight);
+	}
+
+	private void Update()
+	{
+		float moveInput = Input.GetAxisRaw("Horizontal");
+		velocity.x = moveSpeed * moveInput;
+
+		if (controller.info.bottom || controller.info.top)
+		{
+			velocity.y = 0;
+		}
+
+		velocity.y -= gravity * Time.deltaTime;
+
+		if (Input.GetButtonDown("Jump") && controller.info.bottom)
+		{
+			velocity.y = maxJumpVelocity;
+			Debug.Log("Jumped");
+		}
+		if (Input.GetButtonUp("Jump") && velocity.y > minJumpVelocity)
+		{
+			velocity.y = minJumpVelocity;
+			Debug.Log("Stopped Jumping");
+		}
+
+		controller.Move(velocity * Time.deltaTime);
+	}
+}
